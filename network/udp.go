7daa72b52540d199c26c502b40b1987f24a4ca02
Harshmist <ddawsonfitness@gmail.com>
2021-09-16 14:24:02 +0100

    Running App

--- network/udp.go
+++ network/udp.go
@@ -61,47 +61,6 @@ func StartUDP() {
 	}
 }
 
-/////////////////////////////////////////////////////////////////////
-//		switch fields[0] {
-//		case "LIST":
-//			message := "\n"
-//			i := 0
-//			for _, v := range store.Data {
-//				i++
-//				message = message + strconv.Itoa(i) + ": " + v + "\n"
-//			}
-//			reply := []byte(message)
-//			connection.WriteToUDP(reply, addr)
-//		case "POST":
-//			var value string
-//			message := ""
-//			if len(fields) < 2 {
-//				message = "No value added!\n"
-//				connection.WriteToUDP([]byte(message), addr)
-//			}
-//			value = strings.Join(fields[1:], " ")
-//			store.PostChannel <- value
-//			message = fmt.Sprintf("%v added!\n", value)
-//			connection.WriteToUDP([]byte(message), addr)
-//		case "GET":
-//			requested, err := strconv.Atoi(fields[1])
-//			if err != nil {
-//				fmt.Print(err)
-//			}
-//			connection.WriteToUDP([]byte(store.Data[requested]+"\n"), addr)
-//		case "DELETE":
-//			requested, err := strconv.Atoi(fields[1])
-//			if err != nil {
-//				fmt.Print(err)
-//			}
-//			store.DelChannel <- requested
-//		}
-//		if err != nil {
-//			fmt.Print(err)
-//		}
-//	}
-//}
-
 func UDPMsgReceiver(ch chan string, addr *net.UDPAddr, conn *net.UDPConn) {
 	for {
 		select {
